a0302cffecf84c121dfcfb6b85690ce3cb7c1aef
NixInteractive <nixinteractive@yahoo.com>
2018-10-11 02:47:21 -0400

    Create ActionList.cs

---
+++ ActionList.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+//This script will contain all of the basic actions that can be performed in the game.
+//This gets attached to your player object.
+public class ActionList : MonoBehaviour {
+
+    //This is the basic movement action.
+	public void Move(NavMeshAgent agent, RaycastHit hit)
+    {
+        agent.destination = hit.point;
+        Debug.Log("Moving");
+    }
+
+}
