d0df1a0a7888486bca512e186e4418d2ba71a94c
NixInteractive <nixinteractive@yahoo.com>
2018-10-18 09:45:41 -0400

    Update Controller.cs

--- Controller.cs
+++ Controller.cs
@@ -5,55 +5,70 @@ using UnityEngine;
 //This script handles camera/player movement. It gets attached to the Player object
 public class Controller : MonoBehaviour {
 
-    //How fast the camera moves
-    float panSpeed = 7;
+    float panSpeed = 3; //How fast the player moves
+    float panDetect = 15; //The space from the edge of the screen that the mouse will be detected
+    float oldAltitude; //The stored altitude value for the player
+    float altitude; //The current player altitude
 
-    //The space from the edge of the screen that the mouse will be detected
-    float panDetect = 15;
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update () {
 
-        //Stores the player's inputs as floats for movement
-        float moveX = Input.GetAxis("Horizontal") * panSpeed * Time.deltaTime;
-        float moveY = Input.GetAxisRaw("Mouse ScrollWheel");
-        float moveZ = Input.GetAxis("Vertical") * panSpeed * Time.deltaTime;
+        float moveX = Input.GetAxis("Horizontal") * panSpeed * Time.deltaTime; //Left and right movement
+        float moveY = Input.GetAxisRaw("Mouse ScrollWheel"); //Vertical movement
+        float moveZ = Input.GetAxis("Vertical") * panSpeed * Time.deltaTime; //Forward and backward movement
+        float xPos = Input.mousePosition.x; //The x position of the mouse
+        float yPos = Input.mousePosition.y; //The y position of the mouse
 
-        //Grabs the mouse position
-        float xPos = Input.mousePosition.x;
-        float yPos = Input.mousePosition.y;
+        altitude = Terrain.activeTerrain.SampleHeight(transform.position); //Sets the player's altitude value
 
-        //Moves the player up and down
-        if(transform.position.y >= -5 && transform.position.y <= 5)
+        if(oldAltitude != altitude) //Is the stored altitude value different from the current altitude value
         {
-            moveY *= panSpeed;
-            transform.Translate(new Vector3(0, moveY, 0));
+            AltAdjust(); //Calls the AltAdjust() method
+            oldAltitude = altitude; //Sets the stored altitude value to be the current altitude value
         }
 
-        //Handles left and right movement
-        if (Input.GetKey(KeyCode.A) || xPos > 0 && xPos < panDetect)
+        if (transform.position.y >= -10 && transform.position.y <= 1000) //Is the y value of the player transform position between -10 and 1000
         {
-            moveX -= panSpeed;
-        }
-        else if (Input.GetKey(KeyCode.D) || xPos < Screen.width && xPos > Screen.width - panDetect)
-        {
-            moveX += panSpeed;
+            moveY *= 10; //Multiplies the moveY value by 10
+            transform.Translate(new Vector3(0, moveY, 0)); //Moves the player up or down based on the moveY value
         }
 
-        //Handles forward and backwards movement
-        if (Input.GetKey(KeyCode.W) || yPos < Screen.height && yPos > Screen.height - panDetect)
+        if (Input.GetKey(KeyCode.A) || xPos > 0 && xPos < panDetect) //Is the player holding down the A key or is the mouse within 15 pixels of the left edge of the screen?
         {
-            moveZ += panSpeed;
+            moveX -= panSpeed; //Subtract the panSpeed from the moveX value
         }
-        else if (Input.GetKey(KeyCode.S) || yPos > 0 && yPos < panDetect)
+        else if (Input.GetKey(KeyCode.D) || xPos < Screen.width && xPos > Screen.width - panDetect) //Is the player holding down the D key or is the mouse within 15 pixels of the right edge of the screen?
         {
-            moveZ -= panSpeed;
+            moveX += panSpeed; //Add the panSpeed to the moveX value
         }
 
-        //Moves the player using the above values
-        transform.Translate(new Vector3(moveX, Input.GetAxis("Mouse ScrollWheel") * panSpeed, moveZ));
+        if (Input.GetKey(KeyCode.W) || yPos < Screen.height && yPos > Screen.height - panDetect) //Is the player holding down the W key or is the mouse within 15 pixels of the top edge of the screen?
+        {
+            moveZ += panSpeed; //Add the panSpeed to the moveZ value
+        }
+        else if (Input.GetKey(KeyCode.S) || yPos > 0 && yPos < panDetect) //Is the player holding down the S key or is the mouse within 15 pixels of the bottom edge of the screen?
+        {
+            moveZ -= panSpeed; //Subtract the panSpeed from the moveZ value
+        }
 
-        //Clamps the vertical movement
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, 10, 100), transform.position.z);
+        transform.Translate(new Vector3(moveX, Input.GetAxis("Mouse ScrollWheel") * panSpeed, moveZ)); //Moves the player using the above values
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, 10 + altitude, 100 + altitude), transform.position.z); //Clamps the vertical movement
+
+    }
+
+    //Adjusts the player height to follow the terrain
+    public void AltAdjust()
+    {
+        float newHeight = transform.position.y; //stores the player transform's y position
+
+        Vector3 pos = transform.position; //Stores the player transform position 
+
+        pos.y = altitude; //Sets the y position value to equal the current altitude of the player
+	    
+        pos.y += newHeight; //Adds the current y position value to the altitude
+
+        transform.position = pos; //Sets the player's transform position to the new height
     }
 }
+
