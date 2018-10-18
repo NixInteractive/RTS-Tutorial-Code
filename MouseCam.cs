37c90e6e5c4205e0e46bfde0a22534c210c869d5
NixInteractive <nixinteractive@yahoo.com>
2018-10-18 11:14:54 -0400

    Update MouseCam.cs

--- MouseCam.cs
+++ MouseCam.cs
@@ -6,55 +6,40 @@ using UnityEngine;
 //This script gets attached to the main camera that the player controls
 public class MouseCam : MonoBehaviour {
 
-    //The mouse position as a vector2
-    Vector2 mousePos;
+    Vector2 mousePos; //The mouse position as a vector2
+    Vector2 smoothingVector; //The smoothing to be applied to the movement
 
-    //The smoothing to be applied to the movement
-    Vector2 smoothingVector;
+    public float sensitivity = 5f; //Mouse Sensitivity
+    public float smoothing = 2f; //The value to be applied to the smoothingVector
 
-    //Mouse Sensitivity
-    public float sensitivity = 5f;
-
-    //The value to be applied to the smoothingVector
-    public float smoothing = 2f;
-
-    //Player GameObject (Duh :p)
-    public GameObject Player;
+    private GameObject Player; //Player GameObject (Duh :p)
 
     // Use this for initialization
     void Start () {
 
-        //Retrieves the player object
-        Player = GameObject.FindGameObjectWithTag("Player");
+        Player = GameObject.FindGameObjectWithTag("Player"); //Retrieves the player object
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        //Applies rotation when the player holds down the middle mouse button
-        if (Input.GetMouseButton(2))
+        if (Input.GetMouseButton(2)) //Is the player holding down the middle mouse button?
         {
-            //The direction the mouse is moving
-            var mouseDir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+            var mouseDir = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")); //The direction the mouse is moving
 
-            //Applies smoothing to the sensitivity and applies it to the movement
-            mouseDir = Vector2.Scale(mouseDir, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+            mouseDir = Vector2.Scale(mouseDir, new Vector2(sensitivity * smoothing, sensitivity * smoothing)); //Applies smoothing to the sensitivity and applies it to the movement
 
-            //Defines the smoothing vector with the mouse movement
-            smoothingVector.x = Mathf.Lerp(smoothingVector.x, mouseDir.x, 1f / smoothing);
-            smoothingVector.y = Mathf.Lerp(smoothingVector.y, mouseDir.y, 1f / smoothing);
+            smoothingVector.x = Mathf.Lerp(smoothingVector.x, mouseDir.x, 1f / smoothing); //Defines the x smoothing vector with the mouse movement
+            smoothingVector.y = Mathf.Lerp(smoothingVector.y, mouseDir.y, 1f / smoothing); //Defines the y smoothing vector with the mouse movement
 
-            //Adds the movement to the mouse position
-            mousePos += smoothingVector;
+            mousePos += smoothingVector; //Adds the movement to the mouse position
 
-            //Clamps the vertical movement to prevent odd angles
-            mousePos.y = Mathf.Clamp(mousePos.y, -66, 90);
+            mousePos.y = Mathf.Clamp(mousePos.y, -66, 90); //Clamps the vertical movement to prevent odd angles
 
-            //Rotates the camera based on the mouse position
-            transform.localRotation = Quaternion.AngleAxis(-mousePos.y, Vector3.right);
+            transform.localRotation = Quaternion.AngleAxis(-mousePos.y, Vector3.right); //Rotates the camera based on the mouse position
 
-            //Rotates the player to keep movement consistent
-            Player.transform.localRotation = Quaternion.AngleAxis(mousePos.x, Player.transform.up);
+            Player.transform.localRotation = Quaternion.AngleAxis(mousePos.x, Player.transform.up); //Rotates the player to keep movement consistent
         }
     }
 }
+
