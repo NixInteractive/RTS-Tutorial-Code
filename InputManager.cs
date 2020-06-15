8fa41c33c3a6b56055495f39c6f3106f33d6b598
NixInteractive <nixinteractive@yahoo.com>
2020-06-15 17:33:23 -0400

    Update InputManager.cs

--- InputManager.cs
+++ InputManager.cs
@@ -1,58 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
 
 //This Script handles basic user input. It should be attached to the player object
-public class InputManager : MonoBehaviour {
+public class InputManager : MonoBehaviour
+{
+    public ActionList AL; //ActionList Reference
+
+    public static List<ObjectInfo> selectedObjects = new List<ObjectInfo>(); //Currently selected objects
+
+    public Canvas canvas; //canvas reference
+
+    public Image selectionBox; //Selection box
 
     [SerializeField] LayerMask mask; //The layer we want our Raycast to hit
 
     public bool hasPrimary; //Does the player have a primary selected object?
-    public bool circlePlaced = false; //Has the selection circle been placed?
+    public bool canClickGround = true;
 
     public CanvasGroup ObjectPanel; //The unit information panel
 
     public GameObject primaryObject; //The primary selected game object
-    public GameObject selectionCircle; //The actual selection circle
-    public GameObject selectionCirclePrefab; //The prefab for the selection circle
 
     public ObjectInfo selectedInfo; //The primary object's information
 
     private GameObject[] units; //An array of units
 
-    private Collider[] colliders; //An array of colliders that have been selected
+    private Vector3 startPos; //Box start position
 
+    private BoxCollider worldCollider; //Collider for the selection box
 
-	
-	// Update is called once per frame
-	void Update () {
+    private RectTransform RT; //Transform for the box selector
 
+    private bool isSelecting; //Is the player selecting?
+
+    //Called before start
+    void Awake()
+    {
+        //Retrieves the canvas reference
+        if (canvas == null)
+        {
+            canvas = FindObjectOfType<Canvas>();
+        }
+
+        //Creates the selection box
+        if (selectionBox != null)
+        {
+            RT = selectionBox.GetComponent<RectTransform>();
+            RT.pivot = Vector2.one * .5f;
+            RT.anchorMin = Vector2.one * .5f;
+            RT.anchorMax = Vector2.one * .5f;
+            selectionBox.gameObject.SetActive(false);
+        }
+    }
+
+    private void Start()
+    {
+        AL = GetComponent<ActionList>(); //Retrieves the ActionList reference
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         hasPrimary = primaryObject; //If there is a primary object, hasPrimary is true
-
-        if (Input.GetMouseButtonDown(0)) //Is the player left clicking?
-        {
-            LeftClick(); //Calls the LeftClick() method
-        }
-
-        if(Input.GetMouseButton(0) && selectionCircle == null) //Is the player holding the left mouse button while there is no selection circle?
-        {
-            circlePlaced = true; //The selection circle has been placed
-            CircleCreate(); //Calls the CircleCreate method
-        }
-
-        if (Input.GetMouseButtonUp(0)) //Did the player release the left mouse button?
-        {
-            MultiSelect(); //Calls the MultiSelect method
-            Destroy(selectionCircle); //Destroys the selection circle
-            selectionCircle = null; //Removes the reference to the selection circle
-            colliders = null; //Empties the colliders array
-            circlePlaced = false; //The circle is no longer placed
-        }
-
-        if(circlePlaced == true) //Has the circle been placed?
-        {
-            selectionCircle.transform.localScale = new Vector3(selectionCircle.transform.localScale.x + 0.75f, selectionCircle.transform.localScale.y, selectionCircle.transform.localScale.z + 0.75f); //Increase the x and z scale values of the selection circle
-        }
 
         if (primaryObject != null) //Is there a primary object?
         {
@@ -66,92 +78,120 @@ public class InputManager : MonoBehaviour {
             ObjectPanel.blocksRaycasts = false; //Sets the unit panel to not block raycasts
             ObjectPanel.interactable = false; //Sets the unit panel to be not interactable
         }
-    }
 
-    //Creates and places a selection circle where the player clicked
-    public void CircleCreate()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Creates a ray from the camera to where the player clicked
-        RaycastHit hit; //The point the ray hits
-
-        if (Physics.Raycast(ray, out hit, 500, mask)) //Did the ray hit anything on the Layer Mask?
+        //Did the player left click while being able to click the ground and while the move button is not active?
+        if (Input.GetMouseButtonDown(0) && canClickGround && !AL.moveButtonActive)
         {
-            selectionCircle = Instantiate(selectionCirclePrefab, hit.point, new Quaternion(0, 0, 0, 0)); //Creates and places the selection circle
-        }
-    }
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
 
-    //Selects all units within the selection circle
-    public void MultiSelect()
-    {
-        colliders = Physics.OverlapSphere(selectionCircle.transform.position, selectionCircle.transform.localScale.x * 0.9f); //Grabs all of the colliders the selection circle intersects
-
-        foreach (Collider collider in colliders) //For every collider to be selected
-        {
-            if(collider.gameObject.tag == "Selectable") //Is the collider on a selectable object?
+            //Did the raycast hit something?
+            if (Physics.Raycast(ray, out hit))
             {
-                if(collider.GetComponent<ObjectInfo>().isUnit && collider.GetComponent<ObjectInfo>().isPlayerObject) //Is the collider on a unit owned by the player?
+                ObjectInfo OI = hit.collider.GetComponent<ObjectInfo>();
+
+                //Is there anything currently selected?
+                if (OI != null)
                 {
-                    if (!hasPrimary) //Checks to see if the player has no primary object selected
+                    //Is the player holding left shift?
+                    if (Input.GetKey(KeyCode.LeftShift))
                     {
-                        primaryObject = collider.gameObject; //Sets this object to be the primary object
-                        collider.GetComponent<ObjectInfo>().isPrimary = true; //Tells the object it's the primary
-                        selectedInfo = collider.GetComponent<ObjectInfo>(); //Displays this object's information
+                        UpdateSelection(OI, !OI.isSelected); //Add the clicked object to selected units
                     }
+                    else
+                    {
+                        //Replace selected units with the clicked object
+                        ClearSelected();
+                        UpdateSelection(OI, true);
+                    }
+                }
+            }
 
-                    collider.GetComponent<ObjectInfo>().isSelected = true; //Sets the object to be selected
+            startPos = Input.mousePosition; //Set the start position
+            isSelecting = true; //The player is now selecting
+        }
+
+        //Did the player release the left mouse button?
+        if (Input.GetMouseButtonUp(0))
+        {
+            isSelecting = false;
+        }
+
+        //Toggle the selection box based on if the player is selecting
+        selectionBox.gameObject.SetActive(isSelecting);
+
+        //Is the player selecting?
+        if (isSelecting)
+        {
+            //Define new bounds based on current selection box
+            Bounds bounds = new Bounds();
+            bounds.center = Vector3.Lerp(startPos, Input.mousePosition, 0.5f);
+            bounds.size = new Vector3(Mathf.Abs(startPos.x - Input.mousePosition.x), Mathf.Abs(startPos.y - Input.mousePosition.y), 0);
+
+            //Adjust the selection bos image to match the bounds
+            RT.position = bounds.center;
+            RT.sizeDelta = canvas.transform.InverseTransformVector(bounds.size);
+
+            //For each selectable object
+            foreach (ObjectInfo selectable in selectedObjects)
+            {
+                //If it's a unit owned by the player
+                if (selectable.isUnit && selectable.isPlayerObject)
+                {
+                    Vector3 screenPos = Camera.main.WorldToScreenPoint(selectable.transform.position); //Get the unit's position on the screen
+                    screenPos.z = 0; //Set the screen position z to 0
+                    UpdateSelection(selectable, (bounds.Contains(screenPos))); //Update selection based on if the selected object is inside selection box bounds
                 }
             }
         }
     }
 
-
-    //Performs actions based on what the player clicks
-    public void LeftClick()
+    //Updates the current selection
+    void UpdateSelection(ObjectInfo selectedObject, bool value)
     {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); //Creates a ray from the camera to where the player clicks
-        RaycastHit hit; //The object the ray hits
-
-        if(Physics.Raycast(ray, out hit, 100)) //Does the ray hit anything?
+        //Does the selected object's selected value equal the passed in value?
+        if (selectedObject.isSelected != value)
         {
-            if(hit.collider.tag == "Ground") //Did the player click the ground?
+            //Is the value false?
+            if (value == false)
             {
-                selectedInfo.isSelected = false; //Deselects the selected units
-                selectedInfo.isPrimary = false; //Deselects the primary unit
-                primaryObject = null; //Clears the primary object
-
-                units = GameObject.FindGameObjectsWithTag("Selectable"); //Grabs all selectable objects
-
-                foreach (GameObject unit in units) //For each selectable object
+                //Does the player have a primary selected object?
+                if (hasPrimary)
                 {
-                    unit.GetComponent<ObjectInfo>().isSelected = false; //Deselects all selectable objects
+                    //removes this object from the primary if applicable
+                    selectedObject.isPrimary = value;
+                    primaryObject = null;
+                    hasPrimary = value;
+                    AL.actions = null;
                 }
-
-                selectedInfo = null; //Clears out the selected info
             }
-            else if(hit.collider.tag == "Selectable") //Did the player click a selectable object?
+            else
             {
-
-                units = GameObject.FindGameObjectsWithTag("Selectable"); //Grabs all selectable objects
-
-                foreach (GameObject unit in units) //For each selectable object
+                //Does the player not have a primary?
+                if (!hasPrimary)
                 {
-                    unit.GetComponent<ObjectInfo>().isSelected = false; //Deselects all selectable objects
+                    //Assigns this object to be the primary object
+                    selectedObject.isPrimary = value;
+                    primaryObject = selectedObject.gameObject;
+                    hasPrimary = value;
+                    selectedInfo = primaryObject.GetComponent<ObjectInfo>(); //Sets the selected info
+                    AL.actions = selectedInfo.actions;
                 }
-
-                if (hasPrimary) //Is there a primary object?
-                {
-                    selectedInfo.isSelected = false; //Deselects the selected units
-                    selectedInfo.isPrimary = false; //Deselects the primary unit
-                    primaryObject = null; //Clears the primary object
-                }
-
-                primaryObject = hit.collider.gameObject; //Sets the primary object
-
-                selectedInfo = primaryObject.GetComponent<ObjectInfo>(); //Sets the selected info
-
-                selectedInfo.isSelected = true; //Sets the selected unit to be selected
-                selectedInfo.isPrimary = true; //Sets the selected unit to be the primary unit
             }
+            selectedObject.isSelected = value; //Selects or deselects the object based on passed value
         }
+    }
+
+    //Populates the list with selected objects
+    List<ObjectInfo> GetSelected()
+    {
+        return new List<ObjectInfo>(selectedObjects.Where(x => x.isSelected));
+    }
+
+    //Clears the selected objects
+    void ClearSelected()
+    {
+        selectedObjects.ForEach(x => x.isSelected = false);
+        selectedObjects.ForEach(x => x.isPrimary = false);
     }
 }
